54334a42f8b7c8e474d152e593c4632d240239c6
Анастасия <grebneva.nastena@yandex.ru>
2022-09-20 19:33:17 +0300

    FC

---
+++ src/main/java/pro/sky/homeworkException/HomeworkExceptionApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.homeworkException;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkExceptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeworkExceptionApplication.class, args);
+	}
+
+}
